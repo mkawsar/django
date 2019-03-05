e719eef4f7467f592ffc4150ba54a3b5622412bb
mkawsar <kawsar.diu888@gmail.com>
2019-03-05 12:44:47 +0600

    created slug url

--- blog/models.py
+++ blog/models.py
@@ -1,12 +1,18 @@
 from django.db import models
 from django.utils import timezone
 from django.contrib.auth.models import User
+from django.template.defaultfilters import slugify
 
 
 # Create Post models.
 class Post(models.Model):
     title = models.CharField(max_length=255)
     content = models.TextField()
+    slug = models.SlugField(null=True, blank=True)
     date_posted = models.DateTimeField(default=timezone.now)
     author = models.ForeignKey(User, on_delete=models.CASCADE)
 
+    def save(self, *args, **kwargs):
+        if not self.id:
+            self.slug = slugify(self.title)
+        super(Post, self).save(*args, **kwargs)
