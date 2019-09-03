59bb1563d0a9ace186a47823260b45217c47a676
mkawsar <kawsar.diu888@gmail.com>
2019-09-03 13:46:48 +0600

    Created pagination in user list page

--- settings/views.py
+++ settings/views.py
@@ -16,7 +16,7 @@ class UserListView(LoginRequiredMixin, ListView):
     template_name = 'index.html'
     context_object_name = 'users'
     ordering = ['first_name']
-    paginate_by = 5
+    paginate_by = 10
 
     def get_context_data(self, **kwargs):
         context = super().get_context_data(**kwargs)
